83e2bba39aa34397029ae7edf5a9de556a2aa5c1
martheologi <martheo1708@gmail.com>
2021-05-18 23:39:45 +0300

    change graph position

---
+++ models/graph.py
@@ -0,0 +1,10 @@
+
+class Graph:
+    def __init__(self):
+        self.triples = []   
+
+    def addTriple(subject, predicate, object):
+        triple = {}
+        triple["subject"] = subject
+        triple["predicate"] = predicate
+        triple["object"] = object
--- rdflib_tests.py
+++ rdflib_tests.py
@@ -8,8 +8,6 @@ from rdflib import URIRef, BNode, Literal
 from rdflib.serializer import Serializer
 import json 
 
-
-
 # RDF.type
 # = rdflib.term.URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
 
@@ -31,45 +29,48 @@ b0 = URIRef("_:b0")
 b1 = URIRef("_:b1")
 b2 = URIRef("_:b2")
 
+# eoRef1 = URIRef("http://gcmdservices.gsfc.nasa.gov/kms/concept/081f9b6e-d0a0-4f1d-ad8-638189418480")
+# eoRef2 = URIRef("http://gcmdservices.gsfc.nasa.gov/kms/concept/2ce20983-98b2-40b9-bb0e-a08074fb93b3")
+# g.add((eoRef1, RDF.type, schemaorg.Instrument))
+g.add((b0, RDF.type, schemaorg.EarthObservation))
+g.add((b0, schemaorg.eoAquisitionInformation, b1))
+
 # "eoPlatform": {
 #   "@type":"Platform",
 #   "id": "http://gcmdservices.gsfc.nasa.gov/kms/concept/2ce20983-98b2-40b9-bb0e-a08074fb93b3",
 #   "platformSerialIdentifier":"A",
 #   "platformShortName":"Sentinel-2"
 # }
-
 eoPlatform = URIRef("http://gcmdservices.gsfc.nasa.gov/kms/concept/2ce20983-98b2-40b9-bb0e-a08074fb93b3")
+g.add((b1, schemaorg.eoPlatform, eoPlatform))
 g.add((eoPlatform, RDF.type, schemaorg.Platform))
 g.add((eoPlatform, schemaorg.platformSerialIdentifier, Literal("A")))
 g.add((eoPlatform, schemaorg.platformShortName, Literal("Sentinel-2")))
 
-# "eoInstrument": {
-#   "@type" :"Instrument",
-#   "id" : "http://gcmdservices.gsfc.nasa.gov/kms/concept/081f9b6e-d0a0-4f1d-ad8-638189418480",
-#   "name" : "Multi-Spectral Instrument",
-#   "instrumentShortName" : "MSI",
-#   "operationalMode" : "INS-NOBS"
-# }
 
+#     "eoInstrument": {
+#       "@type" :"Instrument",
+#       "id" : "http://gcmdservices.gsfc.nasa.gov/kms/concept/081f9b6e-d0a0-4f1d-ad8-638189418480",
+#       "name" : "Multi-Spectral Instrument",
+#       "instrumentShortName" : "MSI",
+#       "operationalMode" : "INS-NOBS"
 eoInstrument = URIRef("http://gcmdservices.gsfc.nasa.gov/kms/concept/081f9b6e-d0a0-4f1d-ad8-638189418480")
-g.add((eoPlatform, RDF.type, schemaorg.Instrument))
+g.add((b1, schemaorg.eoInstrument, eoInstrument))
 g.add((eoInstrument, schemaorg.instrumentShortName, Literal("MSI")))
 g.add((eoInstrument, schemaorg.name, Literal("Multi-Spectral Instrument")))
 g.add((eoInstrument, schemaorg.operationalMode, Literal("INS-NOBS")))
 
-
 # "eoAcquisitionParameters": {
-#   "@type" : "AcquisitionParameters",
-#   "acquisitionType" : "NOMINAL",
-#   "ascendingNodeDate" : "2018-11-07T16:36:06.154Z",
-#   "acquisitionSubType" : "GS2A_20181107T105231_017637_N02.07",
-#   "orbitNumber" : "17637",
-#   "orbitDirection" : "DESCENDING",
-#   "beginningDateTime" : "2018-11-07T10:52:31.025Z",
-#   "endingDateTime" : "2018-11-07T10:52:31.025Z",
-#   "tileId" : "31UFU"
-# }
-
+#       "@type" : "AcquisitionParameters",
+#       "acquisitionType" : "NOMINAL",
+#       "ascendingNodeDate" : "2018-11-07T16:36:06.154Z",
+#       "acquisitionSubType" : "GS2A_20181107T105231_017637_N02.07",
+#       "orbitNumber" : "17637",
+#       "orbitDirection" : "DESCENDING",
+#       "beginningDateTime" : "2018-11-07T10:52:31.025Z",
+#       "endingDateTime" : "2018-11-07T10:52:31.025Z",
+#       "tileId" : "31UFU"
+g.add((b1, schemaorg.AcquisitionParameters, b2))
 g.add((b1, RDF.type, schemaorg.AcquisitionParameters))
 g.add((b2, schemaorg.acquisitionType, Literal("NOMINAL")))
 g.add((b2, schemaorg.ascendingNodeDate, Literal("2018-11-07T16:36:06.154Z")))
@@ -80,19 +81,9 @@ g.add((b2, schemaorg.beginningDateTime, Literal("2018-11-07T10:52:31.025Z")))
 g.add((b2, schemaorg.endingDateTime, Literal("2018-11-07T10:52:31.025Z")))
 g.add((b2, schemaorg.tileId, Literal("31UFU")))
 
-g.add((b1, schemaorg.eoPlatform, eoPlatform))
-g.add((b1, schemaorg.eoInstrument, eoInstrument))
-g.add((b1, schemaorg.AcquisitionParameters, b2))
-
-g.add((b0, schemaorg.eoAquisitionInformation, b1))
-g.add((b0, RDF.type, schemaorg.EarthObservation))
 
 
-
-# print("--- printing raw triples ---")
-# for s, p, o in g:
-#     print(s, p, o)
 
 f = open("earthobservation.json", "wb")
-f.write(g.serialize(format="json-ld", indent=2))
+f.write(g.serialize(format="json-ld", indent=4))
 f.close()
