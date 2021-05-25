6af3a2238ff5799effaeb33f1abad6f55cea8f32
martheologi <martheo1708@gmail.com>
2021-05-25 23:44:04 +0300

    done with graph

--- tests.py
+++ tests.py
@@ -59,44 +59,40 @@ print("-------------------------------------------------------------------------
 for key in result2:
     print(key, " : ", result2[key])
 
+##########MAPPINGGGGGG########
+map_file = open("./inputs/mapping.json", "r")
+map_data = json.load(map_file)
+print(".......................................................................................................")
+print(".......................................................................................................")
+print(map_data)
+print(".......................................................................................................")
+print(".......................................................................................................")
+
+
 fnType = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
 fnblankNodeType = "blank node"
 
-b1 = Node(fnblankNodeType, "b1")
-pred = Node("IRI", "http://schema.org/eoInstrument")
+b0 = Node(fnblankNodeType, "_:b0")
+b1 = Node(fnblankNodeType, "_:b1")
+b2 = Node(fnblankNodeType, "_:b2")
+
+Instrument_pred = Node("IRI", "http://schema.org/eoInstrument")
 eoInstrument = Node("IRI", "http://gcmdservices.gsfc.nasa.gov/kms/concept/081f9b6e-d0a0-4f1d-ad8-638189418480")
+
+Platform_pred = Node("IRI", "http://schema.org/eoPlatform")
+eoPlatform = Node("IRI", "http://gcmdservices.gsfc.nasa.gov/kms/concept/2ce20983-98b2-40b9-bb0e-a08074fb93b3")
+
+AcqInfo_pred = Node("IRI", "http://schema.org/eoAcquisitionInformation")
+AcqParam_pred = Node("IRI", "http://schema.org/eoAcquisitionParameters")
 
 graph = Graph()
 
-graph.addTriple(b1, pred, eoInstrument)
-
-#     "eoInstrument": {
-#       "@type" :"Instrument",
-#       "id" : "http://gcmdservices.gsfc.nasa.gov/kms/concept/081f9b6e-d0a0-4f1d-ad8-638189418480",
-#       "name" : "Multi-Spectral Instrument",
-#       "instrumentShortName" : "MSI",
-#       "operationalMode" : "INS-NOBS"
-
-graph.addTriple(eoInstrument, Node("IRI", "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), Node("IRI", "http://schema.org/Instrument"))
-graph.addTriple(eoInstrument, Node("IRI", "http://schema.org/eoInstrumentShortName"), Literal("MSI"))
-graph.addTriple(eoInstrument, Node("IRI", "http://schema.org/name"), Literal("Multi-Spectral Instrument"))
-graph.addTriple(eoInstrument, Node("IRI", "http://schema.org/operationalMode"), Literal("INS-NOBS"))
-
-# "eoPlatform": {
-#   "@type":"Platform",
-#   "id": "http://gcmdservices.gsfc.nasa.gov/kms/concept/2ce20983-98b2-40b9-bb0e-a08074fb93b3",
-#   "platformSerialIdentifier":"A",
-#   "platformShortName":"Sentinel-2"
-# }
-
-b1 = Node(fnblankNodeType, "b1")
-pred = Node("IRI", "http://schema.org/eoPlatform")
-eoPlatform = Node("IRI", "http://gcmdservices.gsfc.nasa.gov/kms/concept/2ce20983-98b2-40b9-bb0e-a08074fb93b3")
-
-graph.addTriple(b1, pred, eoPlatform)
-graph.addTriple(eoPlatform, Node("IRI", "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"),  Node("IRI", "http://schema.org/Platform"))
-graph.addTriple(eoPlatform, Node("IRI", "http://schema.org/platformSerialIdentifier"), Literal("A"))
-graph.addTriple(eoPlatform, Node("IRI", "http://schema.org/platformShortName"), Literal("Sentinel-2"))
+graph.addTriple(b0, Node("IRI",fnType), Node("IRI", "http://schema.org/EarthObservation"))
+graph.addTriple(b0, AcqInfo_pred, b1)
+graph.addTriple(b1, Node("IRI",fnType), Node("IRI", "http://schema.org/AcquisitionInformation"))
+graph.addTriple(b1, Instrument_pred, eoInstrument)
+graph.addTriple(b1, Platform_pred, eoPlatform)
+graph.addTriple(b1, AcqParam_pred, b2)
 
 # "eoAcquisitionParameters": {
 #       "@type" : "AcquisitionParameters",
@@ -108,10 +104,8 @@ graph.addTriple(eoPlatform, Node("IRI", "http://schema.org/platformShortName"), 
 #       "beginningDateTime" : "2018-11-07T10:52:31.025Z",
 #       "endingDateTime" : "2018-11-07T10:52:31.025Z",
 #       "tileId" : "31UFU"
-b2 = Node(fnblankNodeType, "b2")
-
-graph.addTriple(b1, Node("IRI", "http://schema.org/AcquisitionParameters"), b2)
-graph.addTriple(b1, Node("IRI", "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), Node("IRI", "http://schema.org/AcquisitionParameters"))
+# }
+graph.addTriple(b2, Node("IRI",fnType), Node("IRI", "http://schema.org/AcquisitionParameters"))
 graph.addTriple(b2, Node("IRI", "http://schema.org/acquisitionType"), Literal("NOMINAL"))
 graph.addTriple(b2, Node("IRI", "http://schema.org/ascendingNodeDate"), Literal("2018-11-07T16:36:06.154Z"))
 graph.addTriple(b2, Node("IRI", "http://schema.org/acquisitionSubType"), Literal("GS2A_20181107T105231_017637_N02.07"))
@@ -121,12 +115,36 @@ graph.addTriple(b2, Node("IRI", "http://schema.org/beginningDateTime"), Literal(
 graph.addTriple(b2, Node("IRI", "http://schema.org/endingDateTime"), Literal("2018-11-07T10:52:31.025Z"))
 graph.addTriple(b2, Node("IRI", "http://schema.org/tileId"), Literal("31UFU"))
 
+#     "eoInstrument": {
+#       "@type" :"Instrument",
+#       "id" : "http://gcmdservices.gsfc.nasa.gov/kms/concept/081f9b6e-d0a0-4f1d-ad8-638189418480",
+#       "name" : "Multi-Spectral Instrument",
+#       "instrumentShortName" : "MSI",
+#       "operationalMode" : "INS-NOBS"
+graph.addTriple(eoInstrument, Node("IRI",fnType), Node("IRI", "http://schema.org/Instrument"))
+graph.addTriple(eoInstrument, Node("IRI", "http://schema.org/eoInstrumentShortName"), Literal("MSI"))
+graph.addTriple(eoInstrument, Node("IRI", "http://schema.org/name"), Literal("Multi-Spectral Instrument"))
+graph.addTriple(eoInstrument, Node("IRI", "http://schema.org/operationalMode"), Literal("INS-NOBS"))
+
+# "eoPlatform": {
+#   "@type":"Platform",
+#   "id": "http://gcmdservices.gsfc.nasa.gov/kms/concept/2ce20983-98b2-40b9-bb0e-a08074fb93b3",
+#   "platformSerialIdentifier":"A",
+#   "platformShortName":"Sentinel-2"
+# }
+graph.addTriple(eoPlatform, Node("IRI",fnType),  Node("IRI", "http://schema.org/Platform"))
+graph.addTriple(eoPlatform, Node("IRI", "http://schema.org/platformSerialIdentifier"), Literal("A"))
+graph.addTriple(eoPlatform, Node("IRI", "http://schema.org/platformShortName"), Literal("Sentinel-2"))
+
+
+
 graph.get()
 
 g = jsonld.from_rdf(graph.get())
 
 compacted = jsonld.compact(g, "https://schema.org/docs/jsonldcontext.jsonld")
-print(json.dumps(compacted, indent=2))
+
+# print(json.dumps(compacted, indent=2))
 
 
 print()
