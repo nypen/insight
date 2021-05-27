c28c36c1e035e4bc4dc316a3b59f398ee62aa50b
penny <ppapadima@indeavor.com>
2021-05-28 00:17:36 +0300

    Remove mapping from collector

--- eoCollector.py
+++ eoCollector.py
@@ -5,13 +5,10 @@ class EOCollector:
         d = data['d']
         results = d['results']
         eoDictionary = {}
-     
-        mapping_file = open("./inputs/mapping.json", "r")
-        mapping_data = json.load(mapping_file)
 
         for info in results:
             metadata = info['__metadata']
             if(info['Id'] in sentinelProperties):
-                eoDictionary[mapping_data[info['Id']]] = info['Value']
+                eoDictionary[info['Id']] = info['Value']
 
         return eoDictionary
