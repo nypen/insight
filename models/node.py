74ea51192a1d34e06caf338e7674260b05f431cd
penny <ppapadima@indeavor.com>
2021-05-28 00:16:57 +0300

    Randomize blank nodes

--- models/node.py
+++ models/node.py
@@ -1,8 +1,13 @@
 class Node:
+    count = 0
     def __init__(
         self,
         nodeType,
-        nodeValue,
+        nodeValue=None,
     ):
         self.type = nodeType
         self.value = nodeValue
+
+        if(self.value==None):
+            self.value = "_:b{}".format(Node.count)
+            Node.count+=1
