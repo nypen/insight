6cb92bb5a44fba80d81ffcaf4077240f811846e1
penny <ppapadima@indeavor.com>
2021-06-06 00:14:33 +0300

    Add login credentials to request

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
+# https://flask.palletsprojects.com/en/2.0.x/quickstart/
 # $env:FLASK_APP="app"
 # python -m flask run
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -15,6 +16,17 @@ CORS(app)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route('/api/products/<product_id>')
+@app.route('/api/products/<product_id>', methods=['POST'])
 def get_product(product_id):
-  return jsonify({"result": "You requested jsonld for " + product_id})
+  if request.method == 'POST':
+    credentials = request.get_json()
+
+    response = {
+      "result": {
+        "id": product_id,
+        "username": credentials["username"],
+        "password": credentials["password"]
+      }
+    }
+
+    return jsonify(response)
