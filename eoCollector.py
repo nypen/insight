e1e44831dc2a6f3bf52fe7e192bf5e4a3d723879
penny <ppapadima@indeavor.com>
2021-06-09 17:24:58 +0300

    Add ability to collect all properties from Copernicus and then filter out the unwanted

--- eoCollector.py
+++ eoCollector.py
@@ -1,14 +1,33 @@
 import json
 class EOCollector:
 
-    def collect(data, sentinelProperties):
+    # returns a dictionary containing all the data we can get
+    # from Copernicus
+    def collect_all(data):
         d = data['d']
         results = d['results']
         eoDictionary = {}
 
         for info in results:
             metadata = info['__metadata']
-            if(info['Id'] in sentinelProperties):
-                eoDictionary[info['Id']] = info['Value']
+            eoDictionary[info['Id']] = info['Value']
+
+        return eoDictionary
+
+    # returns a dictionary containing only the data 
+    # given in sentinelProperties
+
+    def collect(data, sentinelProperties=None):
+        d = data['d']
+        results = d['results']
+
+        eoDictionary = EOCollector.collect_all(data)
+
+        if(sentinelProperties is not None):
+            updatedEoDictionary = eoDictionary.copy()
+            for prop in eoDictionary:
+                if(prop not in sentinelProperties):
+                    updatedEoDictionary.pop(prop)
+            eoDictionary = updatedEoDictionary
 
         return eoDictionary
