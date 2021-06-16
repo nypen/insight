282942d9755c3b0d73ace52ec056802398e15d8c
penny <ppapadima@indeavor.com>
2021-06-16 19:00:11 +0300

    Update comments

--- server/app.py
+++ server/app.py
@@ -1,10 +1,16 @@
 # https://flask.palletsprojects.com/en/2.0.x/quickstart/
 # pip3 install flask
 # pip3 install flask_cors
-# $env:FLASK_APP="app"   (for powershell)
-# python -m flask run
-# export FLASK_APP=hello   (for bash)
-# flask run
+#
+#   - powershell -
+#
+#   $env:FLASK_APP="app"
+#   python -m flask run
+#
+#   - Bash -
+#
+#   export FLASK_APP=hello
+#   flask run
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
