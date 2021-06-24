e240859d1a0a386b920b3f0b1aad76411ef7934e
penny <ppapadima@indeavor.com>
2021-06-25 00:44:16 +0300

    Update styles

--- client/src/components/appBar.tsx
+++ client/src/components/appBar.tsx
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
     appBar: {
-        height: 50,
+        background: "#000",
     }
 }));
 
--- client/src/components/main.tsx
+++ client/src/components/main.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import { Grid, LinearProgress, Typography } from '@material-ui/core';
+import { Grid, LinearProgress, makeStyles, Typography } from '@material-ui/core';
 import { AppBar } from './appBar';
 import { getUrl } from '../appServices';
 import { RequestFormType } from '../requestForm';
 import { RequestForm } from './requestForm';
 
+const useStyles = makeStyles(() => ({
+    root: {
+        border:"3px solid #66afff",
+        height: "100vh"
+    },
+    requestForm: {
+        border:"3px solid #66a5a3",
+        height: "500px"
+    }
+
+}));
+
 interface MainProps { }
 
 const Main = (props: React.PropsWithChildren<MainProps>) => {
-
     const [error, setError] = React.useState(null);
     const [loading, setLoading] = React.useState(false);
     const [result, setResult] = React.useState(null);
+
+    const classes = useStyles();
 
     const handleonClick = (requestForm: RequestFormType) => {
         setLoading(true);
@@ -47,29 +60,31 @@ const Main = (props: React.PropsWithChildren<MainProps>) => {
 
     return (
         <div>
-            <Grid container justify="space-evenly">
+            <Grid container className={classes.root} justify="center">
                 <Grid item xs={12}>
                     <AppBar />
                 </Grid>
-                <Grid item container xs={6}>
-                    <RequestForm
-                        loading={loading}
-                        onSubmitRequest={handleonClick}
-                    />
-                </Grid>
-                <Grid item xs={6}>
-                    <pre>
-                        {JSON.stringify(result, null, '\t')}
-                    </pre>
-                    {
-                        loading && <LinearProgress color="secondary" />
-                    }
-                    <Typography color="error">
-                        {error && error.message}
-                    </Typography>
+                <Grid item container className={classes.requestForm} xs={10}>
+                    <Grid item xs={5}>
+                        <RequestForm
+                            loading={loading}
+                            submitRequest={handleonClick}
+                        />
+                    </Grid>
+                    <Grid item container justify="center" alignItems="center" xs={5}>
+                        <pre>
+                            {JSON.stringify(result, null, '\t')}
+                        </pre>
+                        {
+                            loading && <LinearProgress color="secondary" />
+                        }
+                        <Typography color="error">
+                            {error && error.message}
+                        </Typography>
+                    </Grid>
                 </Grid>
             </Grid>
-        </div >
+        </div>
     );
 }
 
